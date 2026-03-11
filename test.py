ad54d00a63ea37b801afa899ca5b99efe604ddd7
Ram <ram@Haripriyas-MacBook-Air.local>
2026-03-11 17:01:49 +0100

    Login using Sentistrat Website.

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.write("Hello!")
+st.set_page_config(page_title="SentiStrat")
+
+if not st.user.is_logged_in:
+    st.title("Sentistrat Analytics")
+    if st.button("Login with Sentistrat", type="primary"):
+        st.login("wordpress")
+    st.stop()
+
+st.success("Logged in successfully.")
+st.write("User info:")
+st.json(dict(st.user))
+
+if st.button("Logout"):
+    st.logout()
